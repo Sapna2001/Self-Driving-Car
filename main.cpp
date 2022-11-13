eddb46e89049c96b852e3cc3675662b1322e22fc
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2022-11-13 16:07:31 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,39 @@ using namespace std;
 using namespace cv;
 using namespace raspicam;
 
-int main() {
+Mat frame;
+
+// Resolution for image
+void Setup(int argc, char **argv, RaspiCam_Cv &Camera)
+{
+    Camera.set(CAP_PROP_FRAME_WIDTH, ("-w", argc, argv, 400));
+    Camera.set(CAP_PROP_FRAME_HEIGHT, ("-h", argc, argv, 240));
+    Camera.set(CAP_PROP_BRIGHTNESS, ("-br", argc, argv, 50));
+    Camera.set(CAP_PROP_CONTRAST, ("-co", argc, argv, 50));
+    Camera.set(CAP_PROP_SATURATION, ("-sa", argc, argv, 50));
+    Camera.set(CAP_PROP_GAIN, ("-g", argc, argv, 50));
+    Camera.set(CAP_PROP_FPS, ("-fps", argc, argv, 100));
+}
+
+int main(int argc, char **argv)
+{
     RaspiCam_Cv Camera;
+    Setup(argc, argv, Camera);
+
     cout << "Connecting to camera" << endl;
-    if (!Camera.open()) {
+    if (!Camera.open())
+    {
         cout << "Failed to Connect" << endl;
         return -1;
     }
-    
+
+    while (1)
+    {
+        Camera.grab();
+        Camera.retrieve(frame);
+        imshow("orignal", frame);
+        waitKey(1);
+    }
+
     return 0;
 }
