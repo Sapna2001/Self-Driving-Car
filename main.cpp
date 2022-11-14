c722c8201860d8a3c91fc99c2da343720aad4015
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2022-11-14 12:10:37 +0530

    calculate FPS and convert BGR2RGB

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <opencv2/opencv.hpp>
 #include <raspicam_cv.h>
 #include <iostream>
+#include <chrono>
+#include <ctime>
 
 using namespace std;
 using namespace cv;
 using namespace raspicam;
 
 Mat frame;
+RaspiCam_Cv Camera;
 
 // Resolution for image
 void Setup(int argc, char **argv, RaspiCam_Cv &Camera)
@@ -20,9 +23,16 @@ void Setup(int argc, char **argv, RaspiCam_Cv &Camera)
     Camera.set(CAP_PROP_FPS, ("-fps", argc, argv, 100));
 }
 
+// Capture frame
+void Capture()
+{
+	Camera.grab();
+    Camera.retrieve(frame);
+    cvtColor(frame, frame, COLOR_BGR2RGB);
+}
+
 int main(int argc, char **argv)
 {
-    RaspiCam_Cv Camera;
     Setup(argc, argv, Camera);
 
     cout << "Connecting to camera" << endl;
@@ -34,9 +44,19 @@ int main(int argc, char **argv)
 
     while (1)
     {
-        Camera.grab();
-        Camera.retrieve(frame);
+        auto start = std::chrono::system_clock::now();
+
+        Capture();
+        namedWindow("orignal", WINDOW_KEEPRATIO);
+        moveWindow("orignal", 50, 100);
+        resizeWindow("orignal", 640, 480);
         imshow("orignal", frame);
+
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsedSeconds = end - start;
+        float time = elapsedSeconds.count();
+        int FPS = 1 / t;
+
         waitKey(1);
     }
 
