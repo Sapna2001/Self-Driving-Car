fa380f04491a364a6b050cba817e361c706a5d8d
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2022-11-15 12:40:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,13 +8,16 @@ using namespace std;
 using namespace cv;
 using namespace raspicam;
 
-Mat frame;
+Mat frame, matrix, framePerspective;
 RaspiCam_Cv Camera;
+
+Point2f Source[] = { Point2f(50,200),Point2f(200,200),Point2f(0,240), Point2f(360,240) };
+Point2f Destination[] = { Point2f(60,0),Point2f(300,0),Point2f(60,240), Point2f(300,240) };
 
 // Resolution for image
 void Setup(int argc, char **argv, RaspiCam_Cv &Camera)
 {
-    Camera.set(CAP_PROP_FRAME_WIDTH, ("-w", argc, argv, 400));
+    Camera.set(CAP_PROP_FRAME_WIDTH, ("-w", argc, argv, 360));
     Camera.set(CAP_PROP_FRAME_HEIGHT, ("-h", argc, argv, 240));
     Camera.set(CAP_PROP_BRIGHTNESS, ("-br", argc, argv, 50));
     Camera.set(CAP_PROP_CONTRAST, ("-co", argc, argv, 50));
@@ -26,9 +29,26 @@ void Setup(int argc, char **argv, RaspiCam_Cv &Camera)
 // Capture frame
 void Capture()
 {
-	Camera.grab();
+    Camera.grab();
     Camera.retrieve(frame);
     cvtColor(frame, frame, COLOR_BGR2RGB);
+}
+
+// ROI and perspective vision
+void Perspective()
+{
+	line(frame,Source[0], Source[1], Scalar(0,0,255), 2);
+	line(frame,Source[1], Source[3], Scalar(0,0,255), 2);
+	line(frame,Source[3], Source[2], Scalar(0,0,255), 2);
+	line(frame,Source[2], Source[0], Scalar(0,0,255), 2);
+	
+	line(frame, Destination[0], Destination[1], Scalar(0, 255, 0), 2);
+    	line(frame, Destination[1], Destination[3], Scalar(0, 255, 0), 2);
+    	line(frame, Destination[3], Destination[2], Scalar(0, 255, 0), 2);
+    	line(frame, Destination[2], Destination[0], Scalar(0, 255, 0), 2);
+
+    	matrix = getPerspectiveTransform(Source, Destination);
+    	warpPerspective(frame, framePerspective, matrix, Size(360, 240));
 }
 
 int main(int argc, char **argv)
@@ -47,10 +67,17 @@ int main(int argc, char **argv)
         auto start = std::chrono::system_clock::now();
 
         Capture();
+        Perspective();
+
         namedWindow("orignal", WINDOW_KEEPRATIO);
         moveWindow("orignal", 50, 100);
         resizeWindow("orignal", 640, 480);
         imshow("orignal", frame);
+
+        namedWindow("perspective", WINDOW_KEEPRATIO);
+        moveWindow("perspective", 500, 100);
+        resizeWindow("perspective", 640, 480);
+        imshow("perspective", framePerspective);
 
         auto end = std::chrono::system_clock::now();
         std::chrono::duration<double> elapsedSeconds = end - start;
