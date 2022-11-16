05a167394e9c5f24dbe0cb897f75f3e174d13179
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2022-11-16 11:23:31 +0530

    Create laptop_camera.cpp

---
+++ laptop_camera.cpp
@@ -0,0 +1,88 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+using namespace cv;
+
+Mat frame, matrix, framePerspective;
+VideoCapture cap(0);
+Point2f Source[] = { Point2f(50,200),Point2f(200,200),Point2f(0,240), Point2f(360,240) };
+Point2f Destination[] = { Point2f(60,0),Point2f(300,0),Point2f(60,240), Point2f(300,240) };
+
+using namespace cv;
+
+void Setup(int argc, char** argv)
+{
+    cap.set(CAP_PROP_FRAME_WIDTH, ("-w", argc, argv, 360));
+    cap.set(CAP_PROP_FRAME_HEIGHT, ("-h", argc, argv, 240));
+    cap.set(CAP_PROP_BRIGHTNESS, ("-br", argc, argv, 50));
+    cap.set(CAP_PROP_CONTRAST, ("-co", argc, argv, 50));
+    cap.set(CAP_PROP_SATURATION, ("-sa", argc, argv, 50));
+    cap.set(CAP_PROP_GAIN, ("-g", argc, argv, 50));
+    cap.set(CAP_PROP_FPS, ("-fps", argc, argv, 100));
+}
+
+// Capture frame
+void Capture()
+{
+    cap.grab();
+    cap.retrieve(frame);
+    cvtColor(frame, frame, COLOR_BGR2RGB);
+}
+
+void Perspective()
+{
+    line(frame, Source[0], Source[1], Scalar(0, 0, 255), 2);
+    line(frame, Source[1], Source[3], Scalar(0, 0, 255), 2);
+    line(frame, Source[3], Source[2], Scalar(0, 0, 255), 2);
+    line(frame, Source[2], Source[0], Scalar(0, 0, 255), 2);
+
+    line(frame, Destination[0], Destination[1], Scalar(0, 255, 0), 2);
+    line(frame, Destination[1], Destination[3], Scalar(0, 255, 0), 2);
+    line(frame, Destination[3], Destination[2], Scalar(0, 255, 0), 2);
+    line(frame, Destination[2], Destination[0], Scalar(0, 255, 0), 2);
+
+    matrix = getPerspectiveTransform(Source, Destination);
+    warpPerspective(frame, framePerspective, matrix, Size(360, 240));
+}
+
+int main(int argc, char** argv)
+{
+
+    Mat image, matrix;
+    Setup(argc, argv);
+
+    if (!cap.isOpened()) { 
+        cout << "No video stream detected" << endl;
+        system("pause");
+        return-1;
+    }
+
+    while (true) { 
+        auto start = std::chrono::system_clock::now();
+
+        Capture();
+        Perspective();
+
+        namedWindow("orignal", WINDOW_KEEPRATIO);
+        moveWindow("orignal", 50, 100);
+        resizeWindow("orignal", 640, 480);
+        imshow("orignal", frame);
+
+        namedWindow("perspective", WINDOW_KEEPRATIO);
+        moveWindow("perspective", 500, 100);
+        resizeWindow("perspective", 640, 480);
+        imshow("perspective", framePerspective);
+
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsedSeconds = end - start;
+        float time = elapsedSeconds.count();
+        int FPS = 1 / time;
+
+        waitKey(1);
+    }
+    cap.release();
+    return 0;
+}
