e58b5c44cfa89e72aa49823d97bce73b67a11df3
Arathi Karuna M S <62460488+Arathikaruna@users.noreply.github.com>
2023-03-10 19:50:29 +0530

    Update camera_detection.cpp

--- cpp_code/camera_detection.cpp
+++ cpp_code/camera_detection.cpp
@@ -1,45 +1,53 @@
-#include <opencv2/opencv.hpp>
-#include <raspicam_cv.h>
 #include <iostream>
-#include <chrono>
-#include <ctime>
+#include <string.h>
+#include "opencv2/core/core.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/videoio/videoio.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/video.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 using namespace std;
 using namespace cv;
-using namespace raspicam;
-
+//VideoCapture camera = new Videocapture(0);
 Mat frame;
 
-void Setup(int argc, char** argv, RaspiCam_Cv& Camera) {
-    Camera.set(CAP_PROP_FRAME_WIDTH, ("-w", argc, argv, 400));
-    Camera.set(CAP_PROP_FRAME_HEIGHT, ("-h", argc, argv, 240));
-    Camera.set(CAP_PROP_BRIGHTNESS, ("-br", argc, argv, 50));
-    Camera.set(CAP_PROP_CONTRAST, ("-co", argc, argv, 50));
-    Camera.set(CAP_PROP_SATURATION, ("-sa", argc, argv, 50));
-    Camera.set(CAP_PROP_GAIN, ("-g", argc, argv, 50));
-    Camera.set(CAP_PROP_FPS, ("-fps", argc, argv, 100));
 
-}
-
-
-int main(int argc, char** argv) {
-
-    RaspiCam_Cv Camera;
-    Setup(argc, argv, Camera);
-    cout << "Connecting to camera" << endl;
-
-    if (!Camera.open()) {
-        cout << "Failed to Connect" << endl;
+int main(int argc, char *argv[])
+{
+  Mat image;
+  VideoCapture videoCapture(0);
+  if(!videoCapture.isOpened()) {
+    std::cerr << "failed to open video capture" << endl;
+    return -1;
+  }
+  while(1) {
+    videoCapture >> image;
+    imshow( "image", image );
+    int c = waitKey(1);
+    if(c=='x') {
+      break;
     }
+  }
+  return 0;
+} 
 
-    cout << "Camera Id = " << Camera.getId() << endl;
+     /* Mat output;
+        VideoCapture cap(0);
 
-    while (1) {
-        Camera.grab();
-        Camera.retrieve(frame);
-        imshow("orignal", frame);
-        waitKey(1);
-    }
 
-    return 0;
-}
+        if( !cap.isOpened() )
+        {
+            cout << "Could not initialize capturing...\n";
+            return 0;
+        }
+
+
+        while(1){
+            cap >> output;
+
+            imshow("webcam input", output);
+            char c = (char)waitKey(10);
+            if( c == 27 ) break;
+          } */
+    
