a48ff1952fe2144f97d1451a63fa87cf051c8b56
Arathi Karuna M S <62460488+Arathikaruna@users.noreply.github.com>
2023-03-10 19:45:27 +0530

    Update lane_detection.cpp

--- cpp_code/lane_detection.cpp
+++ cpp_code/lane_detection.cpp
@@ -1,59 +1,65 @@
-#include <opencv2/opencv.hpp>
-#include <raspicam_cv.h>
 #include <iostream>
+#include <string.h>
+#include "opencv2/core/core.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/videoio/videoio.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/video.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include <chrono>
 #include <ctime>
 
 using namespace std;
 using namespace cv;
-using namespace raspicam;
 
-Mat frame, matrix, framePerspective, frameGray, frameThreshold, frameEdge, frameFinal, ROILane;
+Mat frame, matrix, framePerspective, frameGray, frameThreshold, frameEdge, frameFinal, ROILane, camera, image;
+
 int LeftLanePos, RightLanePos, frameCenter, laneCenter, Result;
-RaspiCam_Cv Camera;
 stringstream ss;
 vector<int> histrogramLane;
-Point2f Source[] = {Point2f(40,145),Point2f(360,145),Point2f(10,195), Point2f(390,195)};
-Point2f Destination[] = {Point2f(100,0),Point2f(280,0),Point2f(100,240), Point2f(280,240)};
+Point2f Source[] = {Point2f(40,295),Point2f(596,295),Point2f(0,340), Point2f(636,340)};
+Point2f Destination[] = {Point2f(180,0),Point2f(480,0),Point2f(180,478), Point2f(480,478)};
 
-// Resolution for image
-void Setup(int argc, char** argv, RaspiCam_Cv& Camera) {
+/* Resolution for image
+void Setup(int argc, chr** argv, RaspiCam_Cv& Camera) {
     Camera.set(CAP_PROP_FRAME_WIDTH, ("-w", argc, argv, 400));
     Camera.set(CAP_PROP_FRAME_HEIGHT, ("-h", argc, argv, 240));
-    Camera.set(CAP_PROP_BRIGHTNESS, ("-br", argc, argv, 50));
-    Camera.set(CAP_PROP_CONTRAST, ("-co", argc, argv, 50));
+    Camera.set(CAP_PROP_BRIGHTNESS, ("-br", argc, argv, 60));
+    Camera.set(CAP_PROP_CONTRAST, ("-co", argc, argv, 60));
     Camera.set(CAP_PROP_SATURATION, ("-sa", argc, argv, 50));
     Camera.set(CAP_PROP_GAIN, ("-g", argc, argv, 50));
     Camera.set(CAP_PROP_FPS, ("-fps", argc, argv, 100));
 }
 
 // Capture frame
-void Capture() {
-    Camera.grab();
-    Camera.retrieve( frame);
-    cvtColor(frame, frame, COLOR_BGR2RGB);
-}
+void Capture(int argc, char** argv, VideoCapture &camera) {
+    camera >> frame;
+     
+} */
 
 // ROI and perspective vision
 void Perspective() {
-	line(frame,Source[0], Source[1], Scalar(0,0,255), 2);
-	line(frame,Source[1], Source[3], Scalar(0,0,255), 2);
-	line(frame,Source[3], Source[2], Scalar(0,0,255), 2);
-	line(frame,Source[2], Source[0], Scalar(0,0,255), 2);
+   
+	line(image,Source[0], Source[1], Scalar(0,0,255), 2);
+	line(image,Source[1], Source[3], Scalar(0,0,255), 2);
+	line(image,Source[3], Source[2], Scalar(0,0,255), 2);
+	line(image,Source[2], Source[0], Scalar(0,0,255), 2);
+    
 	
-	line(frame, Destination[0], Destination[1], Scalar(0, 255, 0), 2);
-    	line(frame, Destination[1], Destination[3], Scalar(0, 255, 0), 2);
-    	line(frame, Destination[3], Destination[2], Scalar(0, 255, 0), 2);
-    	line(frame, Destination[2], Destination[0], Scalar(0, 255, 0), 2);
+	line(image, Destination[0], Destination[1], Scalar(0, 255, 0), 2);
+    	line(image, Destination[1], Destination[3], Scalar(0, 255, 0), 2);
+    	line(image, Destination[3], Destination[2], Scalar(0, 255, 0), 2);
+    	line(image, Destination[2], Destination[0], Scalar(0, 255, 0), 2);
 
     	matrix = getPerspectiveTransform(Source, Destination);
-    	warpPerspective(frame, framePerspective, matrix, Size(360, 240));
+    	warpPerspective(image, framePerspective, matrix, Size(560, 478));
+       imshow("perspective frame", image);
 }
 
 // Threshold operations
 void Threshold() {
     cvtColor(framePerspective, frameGray, COLOR_RGB2GRAY);
-    inRange(frameGray, 200, 255, frameThreshold);
+    inRange(frameGray,254 , 255, frameThreshold);
     Canny(frameGray, frameEdge, 900, 900, 3, false);
     add(frameThreshold, frameEdge, frameFinal);
     cvtColor(frameFinal, frameFinal, COLOR_GRAY2RGB);
@@ -89,7 +95,7 @@ void LaneFinder() {
 // Find the center of the lane
 void LaneCenter() {
     laneCenter = (RightLanePos - LeftLanePos) / 2 + LeftLanePos;
-    frameCenter = 244;
+    frameCenter = 125;
 
     line(frameFinal, Point2f(laneCenter, 0), Point2f(laneCenter, 240), Scalar(0, 255, 0), 3);
     line(frameFinal, Point2f(frameCenter, 0), Point2f(frameCenter, 240), Scalar(255, 0, 0), 3);
@@ -97,17 +103,20 @@ void LaneCenter() {
     Result = laneCenter - frameCenter;
 }
 
-int main(int argc, char** argv) {
-    RaspiCam_Cv Camera;
-    Setup(argc, argv, Camera);
-    cout << "Camera Id = " << Camera.getId() << endl;
-
-    if (!Camera.open()) {
-        cout << "Camera connection failed" << endl;
-    }
-
+int main(int argc, char *argv[]) {
+    
+  VideoCapture videoCapture(0);
+  if(!videoCapture.isOpened()) {
+    std::cerr << "failed to open video capture" << endl;
+    return -1;
+  }
     while (1) {
-        Capture();
+        videoCapture >> image;
+    imshow( "image", image );
+    int c = waitKey(1);
+    if(c=='x') {
+      break;
+    }
         Perspective();
         Threshold();
         Histrogram();
@@ -117,43 +126,50 @@ int main(int argc, char** argv) {
         ss.str(" ");
         ss.clear();
         ss << "Result = " << Result;
-        putText(frame, ss.str(), Point2f(1,50), 0,1, Scalar(0,0,255), 2);
+        putText(image, ss.str(), Point2f(1,50), 0,1, Scalar(0,0,255), 2);
+        
       
         // Original frame
         namedWindow("orignal", WINDOW_KEEPRATIO);
         moveWindow("orignal", 50, 100);
         resizeWindow("orignal", 640, 480);
-        imshow("orignal", frame);
+        imshow("orignal", camera);
 
 	// Bird's eye view frame
         namedWindow("perspective", WINDOW_KEEPRATIO);
-        moveWindow("perspective", 500, 100);
+        moveWindow("perspective", 640, 100);
         resizeWindow("perspective", 640, 480);
         imshow("perspective", framePerspective);
-	    
+        
+       namedWindow("threshold", WINDOW_KEEPRATIO);
+        moveWindow("threshold", 80, 100);
+        resizeWindow("threshold", 640, 480);
+        imshow("threshold", frameThreshold); 
+	   
 	// Gray frame
 	namedWindow("gray", WINDOW_KEEPRATIO);
         moveWindow("gray", 80, 100);
         resizeWindow("gray", 640, 480);
         imshow("gray", frameGray);
-	    
+	   
+        
 	// Canny edge detection
 	namedWindow("edge", WINDOW_KEEPRATIO);
         moveWindow("edge", 80, 100);
         resizeWindow("edge", 640, 480);
         imshow("edge", frameEdge);
-        waitKey(1);
-	    
+       // waitKey(1);
+	   
 	// Lane detected frame
 	namedWindow("final frame", WINDOW_KEEPRATIO);
-        moveWindow("final frame", 80, 100);
+        moveWindow("final frame", 50, 560);
         resizeWindow("final frame", 640, 480);
         imshow("final frame", frameFinal);
-	    
-	cout << Result << endl;
+	  
+	//cout << Result << endl; 
   
-        waitKey(1);
-    }
+        waitKey(1); 
+    } 
 
     return 0;
 }
